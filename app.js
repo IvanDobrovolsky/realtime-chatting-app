2d381c6426d70e090792e2688676681e6cd65e9b
IvanDobrovolsky <dobrovolsky94@gmail.com>
2016-05-19 00:49:38 +0300

    Added validation to user requests.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const config = require('./config');
 
 const express = require('express');
 
+//TODO implement for all users not a single one
+let isLogined = false;
+let arePeopleInChat = false;
+
 //Initializing the app
 const app = express();
 config.init(app, express);
@@ -18,8 +22,22 @@ app.post('/submit', (request, response) => {
 
    const body = JSON.stringify(request.body);
 
-   response.end(body);
+   //TODO Check whether people are in chat
+   if(arePeopleInChat){
+      response.render('/chat');
+   } else {
+      //TODO Link is hardcoded for now(Add a real one!)
+      response.render('invitation', {link: 'localhost:4444'})
+   }
 
+});
+
+app.get('/chat', (request, response) => {
+   if(isLogined){
+      //response.render('chat');
+   } else {
+      response.redirect('/login');
+   }
 });
 
 //Handling 404 request
